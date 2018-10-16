315a6227e429cffd44102ad93493002bfc41d305
Dave Ackley <ackley@ackleyshack.com>
2018-10-16 00:24:36 -0600

    Helper method as keyboard commands start proliferating

--- VIDEX-2018/src/com/putable/videx/core/events/KeyboardEventInfo.java
+++ VIDEX-2018/src/com/putable/videx/core/events/KeyboardEventInfo.java
@@ -5,6 +5,14 @@ import java.awt.event.KeyEvent;
 public class KeyboardEventInfo extends IOEvent {
     private final KeyEvent mKeyEvent;
 
+    public boolean isKeyTyped(char ch) {
+        KeyEvent ke = getKeyEvent();
+        if (ke == null) throw new IllegalStateException();
+        if (ke.getID() != ke.KEY_TYPED) return false;
+        char c = ke.getKeyChar();
+        return ch == c;
+    }
+    
     public KeyEvent getKeyEvent() {
         return mKeyEvent;
     }
