b33977aa669ecc28129de342cf665e5676ff7f06
Dave Ackley <ackley@ackleyshack.com>
2018-10-20 05:34:25 -0600

    Support opaque HittableImages
    For a substantial speed up when lots of mages are in use.

--- VIDEX-2018/src/com/putable/videx/core/HittableImage.java
+++ VIDEX-2018/src/com/putable/videx/core/HittableImage.java
@@ -11,52 +11,66 @@ import javax.imageio.ImageIO;
 
 import com.putable.videx.interfaces.VO;
 
-
 public class HittableImage {
 
     private final VO mForVO;
+    private boolean mAlphaHittable = true;
     private BufferedImage mImage = null;
     private BufferedImage mHitmapImage = null;
+    private boolean mLastLoadGood = false;
+
+    public void setAlphaHittable(boolean to) {
+        mAlphaHittable = to;
+    }
+    public boolean isAlphaHittable() {
+        return mAlphaHittable;
+    }
+    public boolean isLastLoadGood() {
+        return mLastLoadGood;
+    }
 
     private void initHitmapImage(VOGraphics2D v2d) {
-        if (mHitmapImage != null) throw new IllegalStateException();
+        if (mHitmapImage != null)
+            throw new IllegalStateException();
         int w = mImage.getWidth();
         int h = mImage.getHeight();
-        mHitmapImage = new BufferedImage(w,h,BufferedImage.TYPE_INT_ARGB);
+        mHitmapImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2d = mHitmapImage.createGraphics();
-        g2d.setColor(new Color(0,true));
+        g2d.setColor(new Color(0, true));
         g2d.fillRect(0, 0, w, h);
         int hittable = v2d.getGraphics2D().getColor().getRGB();
         for (int y = 0; y < h; ++y) {
             for (int x = 0; x < w; ++x) {
                 int pix = mImage.getRGB(x, y);
-                int alpha = (pix>>24) & 0xff;
+                int alpha = (pix >> 24) & 0xff;
                 if (alpha > 127)
                     mHitmapImage.setRGB(x, y, hittable);
             }
         }
     }
-    
+
     public void setImageFromBytesIfPossible(String sourceLabel, byte[] bytes) {
         try {
             mImage = ImageIO.read(new ByteArrayInputStream(bytes));
-        }
-        catch (IOException e) {
-            mImage = new BufferedImage(500,100,BufferedImage.TYPE_INT_ARGB);
+            mLastLoadGood = true;
+        } catch (IOException e) {
+            mImage = new BufferedImage(500, 100, BufferedImage.TYPE_INT_ARGB);
             Graphics2D g2d = mImage.createGraphics();
-            g2d.drawString("IMAGE DATA READ FAILED FOR: "+sourceLabel, 10, 10);
+            g2d.drawString("IMAGE DATA READ FAILED FOR: " + sourceLabel, 10,
+                    10);
+            mLastLoadGood = false;
         }
         mHitmapImage = null; // Force rebuild
-        
+
     }
+
     public void setImageFromPathIfPossible(String path) {
         try {
             mImage = ImageIO.read(new File(path));
-        }
-        catch (IOException e) {
-            mImage = new BufferedImage(500,100,BufferedImage.TYPE_INT_ARGB);
+        } catch (IOException e) {
+            mImage = new BufferedImage(500, 100, BufferedImage.TYPE_INT_ARGB);
             Graphics2D g2d = mImage.createGraphics();
-            g2d.drawString("IMAGE LOAD FAILED FOR: "+path, 10, 10);
+            g2d.drawString("IMAGE LOAD FAILED FOR: " + path, 10, 10);
         }
         mHitmapImage = null; // Force rebuild
     }
@@ -69,14 +83,25 @@ public class HittableImage {
         this(forVO);
         setImageFromPathIfPossible(path);
     }
-    
-    
+
     public BufferedImage getImage(VOGraphics2D v2d) {
         if (v2d.isRenderingToHitmap()) {
-            if (mHitmapImage == null) initHitmapImage(v2d);
+            if (mHitmapImage == null)
+                initHitmapImage(v2d);
             return mHitmapImage;
         }
         return mImage;
+    }
+
+    public void drawImage(VOGraphics2D v2d, int x, int y) {
+        if (v2d.isRenderingToHitmap() && !this.isAlphaHittable()) {
+            v2d.getGraphics2D().fillRect(x, y, mImage.getWidth(), mImage.getHeight());
+        } else {
+            BufferedImage bi = getImage(v2d);
+            v2d.getGraphics2D().drawImage(bi, x, y, bi.getWidth(),
+                    bi.getHeight(), null);
+        }
+
     }
 
 }
